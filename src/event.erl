102f7b17a9e484b34e38d61125cdc8af219be119
Yeger-Remy <samyremy.idjer@studenti.unimi.it>
2023-10-27 13:31:17 +0200

    base project

---
+++ src/event.erl
@@ -0,0 +1,55 @@
+-module(event).
+-compile(nowarn_export_all).
+-compile(export_all). %%solo mentre sto testando, da togliere poi
+-record(state, {server,
+				name = "",
+				to_go = 0}).
+
+start(EventName, DateTime) ->
+	spawn(?MODULE, init, [self(), EventName, DateTime]).
+
+start_link(EventName, DateTime) ->
+	spawn_link(?MODULE, init, [self(), EventName, DateTime]).
+
+init(Server, EventName, DateTime) ->
+	loop(#state{server = Server,
+				name = EventName,
+				to_go = time_to_go(DateTime)}).
+				
+loop(S = #state{server = Server, to_go = [T | Next]}) ->
+	receive
+		{Server, Ref, cancel} ->
+			Server ! {Ref, ok}
+	after T*1000 ->
+		if Next =:= [] ->
+				Server ! {done, S#state.name};
+			Next =/= [] ->
+				loop(S#state{to_go=Next})
+			end
+	end.
+
+
+normalize(N) ->
+	Limit = 49*24*60*60,
+	[N rem Limit | lists:duplicate(N div Limit, Limit)].
+
+
+cancel(Pid) ->
+	Ref = erlang:monitor(process, Pid),
+	Pid ! {self(), Ref, cancel},
+	receive
+		{Ref, ok} ->
+			erlang:demonitor(Ref, [flush]),
+			ok;
+		{'DOWN', Ref, process, Pid, _Reason} ->
+			ok
+	end.
+
+time_to_go(TimeOut={{_,_,_}, {_,_,_}}) ->
+	Now = calendar:local_time(),
+	ToGo = calendar:datetime_to_gregorian_seconds(TimeOut) -
+		calendar:datetime_to_gregorian_seconds(Now),
+	Secs = if ToGo > 0  -> ToGo;
+				ToGo =< 0 -> 0
+			end,
+	normalize(Secs).
